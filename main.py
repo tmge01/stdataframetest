a4ad7b5255c9c0445df8187cc511425df1386f40
tmge01 <yos.kajimoto@gmail.com>
2022-10-05 11:07:14 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#dataframe表示テスト
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({'国語テスト': [55, 96, 76, 82,67],
+    '算数テスト': [44, 77, 54, 67, 100],
+    '英語テスト': [67, 54, 76, 91, 68]})
+st.subheader('テスト結果')
+st.dataframe(df)
